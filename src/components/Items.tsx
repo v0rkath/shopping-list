719a8408c203aa6dd35689dbeec8d0b8ab623ea4
v0rkath <grinnedtumblr@gmail.com>
2024-12-05 23:56:54 +0000

    List items styling
    Items now turn grey when marked as completed

--- src/components/Items.tsx
+++ src/components/Items.tsx
@@ -10,7 +10,7 @@ export default function Items(props: {name: string, move: Function}) {
     }
     
     return (
-        <div className={`rounded-3xl bg-sage ${marked ? "bg-neutral-100" : "bg-sage"} p-4 text-left md:inline-flex flex flex-col justify-around mr-4 cursor-pointer mb-4 text-left`} onClick={handleClick}>
+        <div className={`rounded-3xl ${marked ? "bg-neutral-100" : "bg-sage"} p-4 text-left md:inline-flex flex flex-col justify-around mr-4 cursor-pointer mb-4 text-left`} onClick={handleClick}>
 
             {marked ?
             <p className="text-xl font-medium pb-3 pl-1 text-zinc-500"><s>{props.name}</s> <span className="text-sm text-zinc-500">x2</span></p> :
